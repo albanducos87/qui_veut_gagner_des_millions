e8db55642cc2e0b8b2c36483bac59804c8404155
ClementLeCaloch <clemlecaloch@orange.fr>
2022-03-01 15:56:12 +0100

    connexion pdo

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -4,20 +4,10 @@ $db_user = 'root';
 $db_password = 'root';
 $db_db = 'qvgdm_base';
 
-$mysqli = @new mysqli(
-    $db_host,
-    $db_user,
-    $db_password,
-    $db_db
-);
-
-if ($mysqli->connect_error) {
-    echo 'Errno: '.$mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
-    exit();
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=qvgdm_base', $db_user, $db_password);
 }
-
-
-$mysqli->close();
+catch (PDOException $e) {
+    echo 'Echec lors de la connexion : ' . $e->getMessage();
+}
 ?>
