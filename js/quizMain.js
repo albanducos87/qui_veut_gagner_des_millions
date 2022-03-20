3f7aa034cc7f173c3a1cd3ff0679b7ddcca48bdd
ClementLeCaloch <clemlecaloch@orange.fr>
2022-03-20 19:51:49 +0100

    ajout du fond 3d

--- js/quizMain.js
+++ js/quizMain.js
@@ -47,24 +47,23 @@ function Medallion() {
         rot();
     }
     rot = function() {
-        if(rotations < maxrotations) {
-            rotations ++;
+        if (rotations < maxrotations) {
+            rotations++;
             rotateOneDegree();
             window.setTimeout(rot, timeout);
-        }
-        else {
+        } else {
             listener();
         }
     }
     rotateOneDegree = function() {
         degrees += 1;
-        if(degrees > 360) {
+        if (degrees > 360) {
             degrees = degrees - 360;
         }
-        logo.style.transform="rotateY(" + degrees + "deg)";
-        logo.style.webkitTransform="rotateY(" + degrees + "deg)";
-        logo.style.OTransform="rotateY(" + degrees + "deg)";
-        logo.style.MozTransform="rotateY(" + degrees + "deg)";
+        logo.style.transform = "rotateY(" + degrees + "deg)";
+        logo.style.webkitTransform = "rotateY(" + degrees + "deg)";
+        logo.style.OTransform = "rotateY(" + degrees + "deg)";
+        logo.style.MozTransform = "rotateY(" + degrees + "deg)";
     }
 }
 
@@ -75,8 +74,8 @@ function Question() {
     this.rightAnswer;
     this.setAnswers = function(ans, rightAnswer) {
         answers = shuffle(ans);
-        for(i = 0; i < answers.length; i ++) {
-            if(answers[i] == rightAnswer) {
+        for (i = 0; i < answers.length; i++) {
+            if (answers[i] == rightAnswer) {
                 this.rightAnswer = i;
             }
         }
@@ -86,7 +85,8 @@ function Question() {
     }
 
     shuffle = function(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
             // Pick a remaining element...
@@ -108,7 +108,7 @@ function LeaderBoard(onUse5050, onUseTel, onUsePub) {
 
     var joker5050 = document.getElementById("joker_5050");
     var jokerTel = document.getElementById("joker_tel");
-    var jokerPub =  document.getElementById("joker_pub");
+    var jokerPub = document.getElementById("joker_pub");
     joker5050.addEventListener("click", function(event) {
         use5050();
     });
@@ -121,23 +121,23 @@ function LeaderBoard(onUse5050, onUseTel, onUsePub) {
 
     this.setRank = function(rank) {
         var r = document.getElementById("r_" + rank);
-        if(rank > 1) {
-            var rold = document.getElementById("r_" + (rank -1));
+        if (rank > 1) {
+            var rold = document.getElementById("r_" + (rank - 1));
             rold.classList.remove("marked");
         }
         r.classList.add("marked");
     }
     use5050 = function() {
         console.log('oui1');
-        if(!used5050) {
-            if(onUse5050()) {
+        if (!used5050) {
+            if (onUse5050()) {
                 used5050 = true;
                 joker5050.src = "images/joker_5050_x.svg";
             }
         }
     }
     useTel = function() {
-        if(!usedTel) {
+        if (!usedTel) {
             if (onUseTel()) {
                 usedTel = true;
                 jokerTel.src = "images/joker_tel_x.svg";
@@ -145,7 +145,7 @@ function LeaderBoard(onUse5050, onUseTel, onUsePub) {
         }
     }
     usePub = function() {
-        if(!usedPub) {
+        if (!usedPub) {
             if (onUsePub()) {
                 usedPub = true;
                 jokerPub.src = "images/joker_pub_x.svg";
@@ -163,14 +163,14 @@ function QuestionBoard(ocl) {
     ansElement[3] = document.getElementById("ans4");
     var onClickListener = ocl;
     var ansDiv = new Array();
-    for(i = 0; i < 4; i ++) {
+    for (i = 0; i < 4; i++) {
         ansDiv[i] = document.getElementById("element" + i);
     }
     console.log(ansDiv);
-    ansDiv[0].addEventListener("click", function(e) {click(0);});
-    ansDiv[1].addEventListener("click", function(e) {click(1);});
-    ansDiv[2].addEventListener("click", function(e) {click(2);});
-    ansDiv[3].addEventListener("click", function(e) {click(3);});
+    ansDiv[0].addEventListener("click", function(e) { click(0); });
+    ansDiv[1].addEventListener("click", function(e) { click(1); });
+    ansDiv[2].addEventListener("click", function(e) { click(2); });
+    ansDiv[3].addEventListener("click", function(e) { click(3); });
 
     var questionElement = document.getElementById("question");
     this.newestQuestion;
@@ -178,7 +178,7 @@ function QuestionBoard(ocl) {
     this.setQuestion = function(question) {
         this.newestQuestion = question;
         questionElement.innerHTML = this.newestQuestion.name;
-        for(i = 0; i < 4; i ++) {
+        for (i = 0; i < 4; i++) {
             ansElement[i].innerHTML = this.newestQuestion.getAnswers()[i];
         }
     }
@@ -198,7 +198,7 @@ function QuestionBoard(ocl) {
         ansDiv[pos].classList.add("suggest");
     }
     this.clear = function() {
-        for(t = 0; t < 4; t ++) {
+        for (t = 0; t < 4; t++) {
             ansDiv[t].classList.remove("right");
             ansDiv[t].classList.remove("wrong");
             ansDiv[t].classList.remove("suggest");
@@ -213,7 +213,7 @@ function QuestionBoard(ocl) {
 function Game() {
     var questions = new Array();
     answerClick = function(i) {
-        if(clickable) {
+        if (clickable) {
             check(i);
         }
     }
@@ -221,15 +221,14 @@ function Game() {
         if (window.XMLHttpRequest) {
             // code for IE7+, Firefox, Chrome, Opera, Safari
             xmlhttp = new XMLHttpRequest();
-        }
-        else {
+        } else {
             // code for IE6, IE5
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         xmlhttp.onload = function() {
-            var xmlDoc = new DOMParser().parseFromString(xmlhttp.responseText,'text/xml');
+            var xmlDoc = new DOMParser().parseFromString(xmlhttp.responseText, 'text/xml');
             var parsedQuestions = xmlDoc.getElementsByTagName("question");
-            for(x = 0; x < parsedQuestions.length; x ++) {
+            for (x = 0; x < parsedQuestions.length; x++) {
                 questions[x] = new Question();
                 questions[x].rank = parsedQuestions[x].getAttribute("rank");
 
@@ -237,32 +236,32 @@ function Game() {
                 questions[x].setAnswers([parsedQuestions[x].getElementsByTagName("answerWrong")[0].childNodes[0].nodeValue,
                         parsedQuestions[x].getElementsByTagName("answerWrong")[1].childNodes[0].nodeValue,
                         parsedQuestions[x].getElementsByTagName("answerWrong")[2].childNodes[0].nodeValue,
-                        parsedQuestions[x].getElementsByTagName("answerRight")[0].childNodes[0].nodeValue],
+                        parsedQuestions[x].getElementsByTagName("answerRight")[0].childNodes[0].nodeValue
+                    ],
                     parsedQuestions[x].getElementsByTagName("answerRight")[0].childNodes[0].nodeValue);
             }
         }
-        xmlhttp.open("GET","quiz.xml",false);
+        xmlhttp.open("GET", "quiz.xml", false);
         xmlhttp.send();
     }
 
-    randomIntFromInterval = function(min,max) {
-        return Math.floor(Math.random()*(max-min+1)+min);
+    randomIntFromInterval = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
     var medallion = new Medallion();
     var questionBoard = new QuestionBoard(answerClick);
     var leaderBoard = new LeaderBoard(function() {
         parseXML();
-        if(clickable) {
+        if (clickable) {
             var p1 = -1;
             var p2 = -1;
-            while(p1 == -1 | p2 == -1) {
+            while (p1 == -1 | p2 == -1) {
                 var random = randomIntFromInterval(0, 3);
-                if(questionBoard.newestQuestion.rightAnswer != random) {
-                    if(p1 == -1) {
+                if (questionBoard.newestQuestion.rightAnswer != random) {
+                    if (p1 == -1) {
                         p1 = random;
-                    }
-                    else {
-                        if(random != p1) {
+                    } else {
+                        if (random != p1) {
                             p2 = random;
                         }
                     }
@@ -271,8 +270,7 @@ function Game() {
             questionBoard.wrong(p1);
             questionBoard.wrong(p2);
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }, function() {
@@ -333,30 +331,28 @@ function Game() {
 
     check = function(i) {
         clickable = false;
-        if(i == questionBoard.newestQuestion.rightAnswer) {
+        if (i == questionBoard.newestQuestion.rightAnswer) {
             questionBoard.right(i);
-            rank ++;
-            $.get("index.php?page=9",{palier: rank, fini: fini}, console.log('maj réussie'));
-            if(rank <= maxrank) {
+            rank++;
+            $.get("index.php?page=9", { palier: rank, fini: fini }, console.log('maj réussie'));
+            if (rank <= maxrank) {
                 medallion.rotate(function() {
                     questionBoard.clear();
                     clickable = true;
                     newQuestion();
                 });
-            }
-            else {
+            } else {
                 medallion.win(function() {
                     questionBoard.clear();
                 });
             }
-        }
-        else {
+        } else {
             fini = 2;
             questionBoard.right(questionBoard.newestQuestion.rightAnswer);
             questionBoard.wrong(i);
-            $.get("index.php?page=9",{palier: rank, fini: fini}, console.log('maj réussie'));
+            $.get("index.php?page=9", { palier: rank, fini: fini }, console.log('maj réussie'));
             medallion.lose(function() {
-                location.href="index.php?page=6";
+                location.href = "index.php?page=6";
             });
         }
     }
@@ -365,8 +361,8 @@ function Game() {
         newQuestion();
     }
     newQuestion = function() {
-        for(r = 0; r < questions.length; r ++) {
-            if(questions[r].rank == rank) {
+        for (r = 0; r < questions.length; r++) {
+            if (questions[r].rank == rank) {
                 leaderBoard.setRank(rank);
                 questionBoard.setQuestion(questions[r]);
                 break;
--- pages/jeu2.inc.php
+++ pages/jeu2.inc.php
@@ -13,52 +13,60 @@ $questionManager->generateXml();
 
 if (isset($_SESSION['idUtilisateur'])) {
 ?>
+<canvas id="canvas3d" width=“800” height=“600” frameborder=“0” style="border:0;"></canvas>
 
-<div id="main">
-    <div id="logo"><img id="logoimg" width="300px" src="images/logo2.png" /></div>
-    <div id="qanda">
-        <div id="question" class="element"></div>
-        <table id="table">
-            <tr>
-                <td>
-                    <div id="element0" class="element"><span class="goldenLetter">A: </span><span id="ans1"></span></div>
-                </td>
-                <td width="50%">
-                    <div id="element1" class="element"><span class="goldenLetter">B: </span><span id="ans2"></span></div>
-                </td>
-            </tr>
-            <tr>
-                <td width="50%">
-                    <div id="element2" class="element"><span class="goldenLetter">C: </span><span id="ans3"></span></div>
-                </td>
-                <td width="50%">
-                    <div id="element3" class="element"><span class="goldenLetter">D: </span><span id="ans4"></span></div>
-                </td>
-            </tr>
-        </table>
+<div id="jeu">
+    <div id="main">
+    <div id="logo"><img id="logoimg" src="images/logo2.png" /></div>
+        <div id="qanda">
+            <div id="question" class="element"></div>
+            <table id="table">
+                <tr>
+                    <td>
+                        <div id="element0" class="element"><span class="goldenLetter">A: </span><span id="ans1"></span></div>
+                    </td>
+                    <td width="50%">
+                        <div id="element1" class="element"><span class="goldenLetter">B: </span><span id="ans2"></span></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="50%">
+                        <div id="element2" class="element"><span class="goldenLetter">C: </span><span id="ans3"></span></div>
+                    </td>
+                    <td width="50%">
+                        <div id="element3" class="element"><span class="goldenLetter">D: </span><span id="ans4"></span></div>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
+    
+    <div class="score">
+        <div id="ranking">
+            <div id="joker">
+                <img id="joker_5050" class="joker" src="images/joker_5050.svg" height="50"/>
+                <img id="joker_tel" class="joker" src="images/joker_tel.svg" height="50"/>
+                <img id="joker_pub" class="joker" src="images/joker_pub.svg" height="50"/>
+            </div>
+            <?php
+            $i=15;
+            foreach ($niveaux as $n) {
+                $id = "r_".$i;
+                echo "<div id=".$id." class=\"rank\">$i&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⬦&nbsp;&nbsp;&nbsp;&nbsp;€".$n['prix']."</div>";
+                $i--;
+            }
+        } else {
+            header('Location: index.php?page=0');
+        }
+            ?>
+        </div>
+        
+        <div id="retour">
+            <a href="index.php?page=6" class="button primary"> Accueil </a>
+        </div>
+    </div> 
 </div>
 
-
-<div id="ranking">
-    <div id="joker">
-        <img id="joker_5050" class="joker" src="images/joker_5050.svg" height="50"/>
-        <img id="joker_tel" class="joker" src="images/joker_tel.svg" height="50"/>
-        <img id="joker_pub" class="joker" src="images/joker_pub.svg" height="50"/>
-    </div>
-    <?php
-    $i=15;
-    foreach ($niveaux as $n) {
-        $id = "r_".$i;
-        echo "<div id=".$id.">$i&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⬦&nbsp;&nbsp;&nbsp;&nbsp;€".$n['prix']."</div>";
-        $i--;
-    }
-} else {
-    header('Location: accueil');
-}
-    ?>
-</div>
-
-<div id="retour">
-    <a href="home" class="button primary"> Accueil </a>
-</div>
+<script type="module">
+    import { Application } from './runtime.js'; const app = new Application(); app.load('./scene.json');
+</script>
